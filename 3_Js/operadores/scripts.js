4ccd19b45053e4565acb359914a29f76576a611f
Pedro Lucas <plpedreira03@gmail.com>
2025-01-24 15:37:42 -0300

    Add Operadores in 3_JS

---
+++ 3_Js/operadores/scripts.js
@@ -0,0 +1,26 @@
+// 1 - Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// 2 - Ops aritiméticas
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(2 * 5);
+console.log(8 / 5);
+console.log(10 + 3 * 6);
+
+// 3 - Special Numbers 
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(12 * "aaa");
+console.log(typeof NaN);
+
+//4 - Strings
+console.log("Texto");
+console.log("Mais um Texto");
+console.log(typeof"19");
+
+// Símbolos especiais em string
+console.log("text em \n Duas linhas");
+console.log("Espaçamento de \t TAB")
