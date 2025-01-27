0a7836514539a2eef4d545c2dc5f6ba338e4cb78
Pedro Lucas <143295682+vulgopedrin@users.noreply.github.com>
2025-01-27 17:12:16 -0300

    progressing in JS

--- 3_Js/operadores/scripts.js
+++ 3_Js/operadores/scripts.js
@@ -1,28 +1,40 @@
 // 1 - Number
 console.log(typeof 2);
+
 console.log(typeof 5.14);
+
 console.log(typeof -127);
 
 // 2 - Ops aritiméticas
 console.log(2 + 4);
+
 console.log(10 - 5);
+
 console.log(2 * 5);
+
 console.log(8 / 5);
+
 console.log(10 + 3 * 6);
 
 // 3 - Special Numbers 
 console.log(typeof Infinity);
+
 console.log(typeof -Infinity);
+
 console.log(12 * "aaa");
+
 console.log(typeof NaN);
 
 // 4 - Strings
 console.log("Texto");
+
 console.log("Mais um Texto");
+
 console.log(typeof"19");
 
 // 5 - Símbolos especiais em string
 console.log("text em \n Duas linhas");
+
 console.log("Espaçamento de \t TAB");
 
 // 6 - Concatenação
@@ -30,27 +42,53 @@ console.log("oi, " + " tudo " + " bem?");
 
 // 7 - Interpolação
 console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+
 console.log(`Podemos executar qualquer coisa aqui ${console.log("Teste")}`);
 
 // 8 - Booleans
 console.log(true);
+
 console.log(false);
+
 console.log(5 > 20);
+
 console.log(30 > 10);
+
 console.log(typeof false);
 
 // 9 - comparação
 console.log(9 == "9");
+
 console.log(9 === "9");
+
 console.log(9 != "9");
+
 console.log(9 !== "9");
 
 //10 - Operadores lógicos
 console.log(true && true);
+
 console.log(true && false);
+
 console.log(5 > 2 && 2 < 10);
+
 console.log(5 > 2 && "Pedro" === 1);
+
 console.log(5 > 2 || "Pedro" === 1);
+
 console.log(5 < 2 || 5 > 100);
+
 console.log(!true);
+
 console.log(!5 > 2);
+
+// 12 - empty values
+console.log(typeof null, typeof undefined);
+
+console.log(null === undefined);
+
+console.log(null == undefined);
+
+console.log(null == false);
+
+console.log(undefined == false);
