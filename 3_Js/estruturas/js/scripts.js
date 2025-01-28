18120bfc68d613e3ca6e3ffe2b1073857c9ff0f6
Pedro Lucas <143295682+vulgopedrin@users.noreply.github.com>
2025-01-28 15:08:51 -0300

    progressing in JS

---
+++ 3_Js/estruturas/js/scripts.js
@@ -0,0 +1,15 @@
+// 1 - Variáveis
+let nome = "Pedro";
+
+console.log(nome);
+
+nome = "Pedro Lucas";
+
+const idade = 20;
+
+console.log(idade);
+
+console.log(typeof nome);
+
+console.log(typeof idade);
+
--- 3_Js/operadores/Js/scripts.js
+++ 3_Js/operadores/Js/scripts.js
@@ -92,3 +92,13 @@ console.log(null == undefined);
 console.log(null == false);
 
 console.log(undefined == false);
+
+// 13 - mudança de tipos
+
+console.log( 5* null);
+
+console.log("teste" * "opa");
+
+console.log("10" + 1);
+
+console.log("10" - 1);
