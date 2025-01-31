13afc293cd377c4c63a0e91c6528045f8ddcb7ae
Pedro Lucas <143295682+vulgopedrin@users.noreply.github.com>
2025-01-31 15:24:36 -0300

    progressing in JS

--- 3_Js/estruturas/js/scripts.js
+++ 3_Js/estruturas/js/scripts.js
@@ -13,3 +13,17 @@ console.log(typeof nome);
 
 console.log(typeof idade);
 
+// 2 - mais sobre vairáveis
+let a = 10,
+    b = 20,
+    c = 30;
+console.log (a, b, c);
+
+const nomecompleto = "Pedro Lucas";
+const nomecompleto = "João Victor";
+
+console.log(nomecompleto);
+console.log(nomecompleto);
+
+let _teste = "ok";
+let $teste = "ok";
