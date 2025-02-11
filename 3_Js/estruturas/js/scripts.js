42d937be3ad6fbf31767d28648a9013a75c8c625
Pedro Lucas <143295682+vulgopedrin@users.noreply.github.com>
2025-02-11 16:43:08 -0300

    progressing in JS

--- 3_Js/estruturas/js/scripts.js
+++ 3_Js/estruturas/js/scripts.js
@@ -17,7 +17,7 @@ console.log(typeof idade);
 let a = 10,
     b = 20,
     c = 30;
-console.log (a, b, c);
+console.log(a, b, c);
 
 const nomecompleto = "Pedro Lucas";
 
@@ -43,9 +43,9 @@ alert(`numero é igual a ${z}`)
 // 5 - Math
 console.log(Math.max(5, 2, 1, 10));
 
-console.log(Math.floor(5,40));
+console.log(Math.floor(5, 40));
 
-console.log(Math.ceil(5, 14));20
+console.log(Math.ceil(5, 14)); 20
 
 // 6 -  cosnole
 console.log("teste!");
@@ -57,7 +57,7 @@ console.warn("aviso!");
 // 7 - if
 const m = 10;
 
-if(m > 5){
+if (m > 5) {
 
     console.log("M é maior que 5!")
 
@@ -65,13 +65,13 @@ if(m > 5){
 
 const user = "João"
 
-if(user === "João"){
+if (user === "João") {
 
     console.log("o nome do usuario é joão");
 
 }
 
-if(user === "Isabella"){
+if (user === "Isabella") {
 
     console.log("o nome do usuario é Isabella");
 
@@ -81,7 +81,7 @@ console.log(user === "João", user === "Isabella");
 
 // 8 - else
 const loggedIn = false
-if(loggedIn) {
+if (loggedIn) {
     console.log("esta autenticado!");
 } else {
     console.log("Não esta autenticado");
@@ -89,28 +89,61 @@ if(loggedIn) {
 
 const q = 10
 const w = 15
-if(q > 5 && w >20) {
-console.log("Números mais altos!");
-} else{
+if (q > 5 && w > 20) {
+    console.log("Números mais altos!");
+} else {
     console.log("os Números não são mais altos!");
 }
 
 // 9 -elif
-if(1 > 2){
+if (1 > 2) {
     console.log("teste");
-} else if(2 > 3) {
+} else if (2 > 3) {
     console.log("teste 2");
-} else if(5 > 1) {
-    console.log ("agora sim");
+} else if (5 > 1) {
+    console.log("agora sim");
 }
 
 const userName = "Pedro"
 const userAge = 20
 
-if(userName === "José") {
+if (userName === "José") {
     console.log("bem vindo Joés!");
 } else if (userName === "Pedro" && userAge === 20) {
     console.log("Bem vindo Pedro!");
 } else {
     console.log("Alguma condição não foi atingida !");
+}
+
+// 10 - while
+let p = 0
+
+while (p < 5) {
+    console.log(`Repetindo ${p}`);
+    p = p + 1;
+}
+
+// loop infinito
+// let x =10;
+
+// while(x > 5) {
+//     console.log (`imprimindo ${x}`);
+// }
+
+// 11 - do while
+let o = 10
+
+do {
+    console.log(`valor de ${o}`);
+    o--;
+} while (o > 1);
+
+// 12 - for
+for (let t = 0; t < 10; t++) {
+    console.log("repetindo algo...");
+}
+
+let r = 10;
+for (r; r > 0; r = r - 1) {
+    console.log(`o r está diminuindo ${r}`);
 }
